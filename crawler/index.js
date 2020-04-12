32f807df27f7b301c0720c6c8989ec159a384b3f
Chris Spiegl <chris@chrisspiegl.com>
2020-04-12 20:17:03 +0200

    Running crawler with node-cron instead of system cron. More self contained this way.

--- crawler/index.js
+++ crawler/index.js
@@ -7,7 +7,9 @@ const log = debug(`${config.slug}:crawler`)
 log.log = console.log.bind(console)
 const error = debug(`${config.slug}:crawler:error`)
 
+const cron = require('cron')
 const moment = require('moment-timezone')
+const pLimit = require('p-limit')
 moment.tz.setDefault('UTC')
 
 const crawlers = {
@@ -17,9 +19,12 @@ const crawlers = {
 
 const models = require('../database/models')
 
-const start = async () => {
-  log('Starting Crawler')
-  await models.init()
+const configParallelCrawlers = 1
+
+const run = async () => {
+  log('Running Crawler')
+
+  const pLimiter = pLimit(configParallelCrawlers)
 
   const manufacturers = await models.Manufacturer.findAll({
     where: {
@@ -45,13 +50,48 @@ const start = async () => {
 
   if (manufacturers.length === 0) log('Nothing to crawl, already finished with everything (or nothing enabled)')
 
-  for await (const manufacturer of manufacturers) {
-    log(`Crawling ${manufacturer.name}`)
-    await crawlers[manufacturer.slug].start(manufacturer)
-  }
+  const promises = manufacturers.map((manufacturer) => {
+    return pLimiter(async () => crawlers[manufacturer.slug].start(manufacturer))
+  })
+
+  await Promise.all(promises)
 
   log('Finished Crawling All')
-  process.exit()
 }
 
+
+const start = async () => {
+  log('Starting Crawler')
+  await models.init()
+  const CronJob = cron.CronJob;
+  const job = new CronJob('0 4 * * * *', run, null, true, 'UTC', this, true);
+  job.start();
+}
 start()
+
+// Graceful shutdown
+process.on('SIGINT', async () => {
+  const cleanUp = async () => {
+    // Clean up other resources like DB connections
+    log('closing database connection')
+    await models.sequelize.close()
+  }
+
+  // Force close server after 5secs
+  setTimeout(async (e) => {
+    log('Forcing server close !!!', e)
+
+    await cleanUp()
+    process.exit(1)
+  }, 10000) // 10 seconds
+
+  log('Closing crawler...')
+
+  await cleanUp()
+  process.exit()
+})
+
+process.on('unhandledRejection', async (reason, promise) => {
+  error('unhandledRejection', reason.stack || reason, promise)
+  pnotice(`unhandledRejection:\n${JSON.stringify(reason)}`, 'ERROR')
+})
